a18792f6ae02b4377322b8ff8816657ac24fc00c
Lanskikh Vitaly <35953088+Bladeflamy@users.noreply.github.com>
2019-06-04 09:46:39 +0300

    Update form.php

--- form.php
+++ form.php
@@ -3,6 +3,7 @@
 	<head>
 		<meta charset="utf-8">
 		<title>Форма заказаа</title>
+		<!-- Тут подключил библеотеку, выбрать способ -->
 		<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 		<script src="ajax.js"></script>
 	</head>
